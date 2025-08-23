8d4f429c0807e5beab0760a1d2eb35cf44769ec4
nico <ni.premont@gmail.com>
2025-08-24 00:06:45 +0200

    basic structures

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,7 +6,7 @@
 /*   By: npremont <npremont@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 11:35:52 by npremont          #+#    #+#             */
-/*   Updated: 2025/08/23 12:06:09 by npremont         ###   ########.fr       */
+/*   Updated: 2025/08/23 14:08:38 by npremont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@
 # define SMALL_UNIT     128
 # define MEDIUM_UNIT    1024
 
-# define DEBUG 0
+# define DEBUG 1
 
+# include <unistd.h>
 # include <stdlib.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <sys/mman.h>
 
 typedef enum
 {
@@ -29,26 +33,26 @@ typedef enum
 
 typedef struct s_header
 {
-    size_t      size;
-    bool        is_free;
+    size_t              size;
+    bool                is_free;
 
-    t_header*   next;
+    struct s_header*    next;
 }   t_header;
 
 typedef struct s_zone
 {
-    void*       start;
-    void*       end;
-    size_t      block_size;
-    t_header*   allocated_list;
-    t_header*   free_list;
+    void*               start;
+    void*               end;
+    size_t              block_size;
+    struct s_zone*      allocated_list;
+    struct s_zone*      free_list;
 }   t_zone;
 
 typedef struct s_large_zone
 {
-    size_t          size;
-    size_t          r_size;
-    t_large_zone*   next;
+    size_t                  size;
+    size_t                  r_size;
+    struct s_large_zone*    next;
 }   t_large_zone;
 
 typedef struct s_malloc_data
--- src/malloc.c
+++ src/malloc.c
@@ -6,13 +6,59 @@
 /*   By: npremont <npremont@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 12:06:25 by npremont          #+#    #+#             */
-/*   Updated: 2025/08/23 12:07:13 by npremont         ###   ########.fr       */
+/*   Updated: 2025/08/23 14:09:14 by npremont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/malloc.h"
 
-void*   malloc(size_t size)
+static t_malloc_data*  g_data;
+
+t_malloc_data*  get_data()
 {
-    
+    if (!g_data)
+    {
+        g_data = mmap(NULL, sizeof(t_malloc_data),
+                    PROT_READ | PROT_WRITE,
+                    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+        if (g_data == MAP_FAILED) {
+            perror("mmap failed\n");
+            return NULL;
+        }
+
+        g_data->s_zone = NULL;
+        g_data->m_zone = NULL;
+        g_data->l_zone = NULL;
+        g_data->total_allocated = 0;
+    }
+
+    if (DEBUG)
+        printf("Total memory allocated: %lu\n", g_data->total_allocated);
+
+    return (g_data);
+}
+
+void*   ft_malloc(size_t size)
+{
+    t_malloc_data* data = get_data();
+    if (!data) return (NULL);
+
+    if (DEBUG)
+        printf("pageSize: %d\n", getpagesize());
+
+    // if (size <= SMALL_UNIT)
+    //     return handleSmallUnit(size, data);
+    // else if (size > SMALL_UNIT && size <= MEDIUM_UNIT)
+    //     return handleMediumUnit(size, data);
+    // else 
+    //     return handleBigUnit(size, data);
+
+    return (NULL);
+}
+
+int main()
+{
+    void *p = ft_malloc(30);
+    return(1);
 }
